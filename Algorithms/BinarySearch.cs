30fad1052a16ef8b75bb01a8a11197d2495f66f8
Barallobre <cristianbarallobrerua@gmail.com>
2024-02-09 23:26:19 +0100

    binary search

---
+++ Algorithms/BinarySearch.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Algorithms
+{
+    public static class BinarySearch
+    {
+
+        public static void BinarySearchAlgorithm(int[] listOfItems, int myNumber)
+        {
+            Console.WriteLine("número a buscar: " + myNumber);
+            int stepsToFindTheNumber = 0;
+
+            int high = listOfItems.Length - 1;
+            int low = 0;
+            int mid = 0;
+
+            while (listOfItems[mid] != myNumber)
+            {
+
+                mid = (high + low) / 2;
+
+                if (listOfItems[mid] < myNumber)
+                {
+                    low = mid + 1;
+
+                }
+                if (listOfItems[mid] > myNumber)
+                {
+                    high = mid - 1;
+                }
+
+                stepsToFindTheNumber++;
+            }
+            Console.WriteLine(stepsToFindTheNumber);
+        }
+    }
+}
+
---
+++ Algorithms/Program.cs
@@ -0,0 +1,14 @@
+﻿using Algorithms;
+
+int[] listOfItems = new int[128];
+
+for (int i = 0; i < listOfItems.Length; i++)
+{
+    listOfItems[i] = i + 1;
+    Console.WriteLine(listOfItems[i]);
+}
+
+Console.WriteLine("Introduzca el número a buscar");
+int myNumber = int.Parse(Console.ReadLine());
+
+BinarySearch.BinarySearchAlgorithm(listOfItems, myNumber);
